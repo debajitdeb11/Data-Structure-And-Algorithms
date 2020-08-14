6e4524e78983ad73ae6c2a769f2550d7a69cc2dc
Debajit Deb <debajitdeb11@gmail.com>
2020-08-15 01:25:33 +0530

    Mathematics Added

---
+++ Mathematics/ComputingPower/Exponentential.java
@@ -0,0 +1,35 @@
+/* Brute Force Approach :
+
+    Finding the exponentation of x ^ n 
+
+    Time Complexitity: O(n)
+    
+*/
+
+public class Exponentential {
+
+    public static void main(String[] args) {
+
+        System.out.println(computeExponential(2, 3));
+
+    }
+
+    static long computeExponential(int x, int n) {
+
+        long result = 1;
+
+        if (n == 0)
+            return (long) 1;
+
+        if (n == 1)
+            return (long) n;
+
+        for (int i = 1; i <= n; i++) {
+            result = result * x;
+        }
+
+        return result;
+
+    }
+
+}
---
+++ Mathematics/ComputingPower/MethodOfExponentation.java
@@ -0,0 +1,37 @@
+
+/* 
+
+Reccursive Solution:
+
+Time Complexitity: O(log n) 
+
+*/
+
+public class MethodOfExponentation {
+
+    public static void main(String[] args) {
+
+        System.out.println(expt(2, 32));
+
+    }
+
+    static long expt(int x, int n) {
+
+        // Base Case:
+        if (n == 0)
+            return 1;
+
+        if (n == 1)
+            return x;
+
+        long temp = expt(x, n / 2);
+
+        temp = temp * temp;
+
+        if (n % 2 != 0)
+            return temp * x;
+
+        return temp;
+    }
+
+}

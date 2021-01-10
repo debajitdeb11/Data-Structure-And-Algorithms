febc719cc3bb28e072492e9b1a3eb44abf3b6174
Debajit Deb <debajitdeb11@gmail.com>
2021-01-10 19:55:13 +0530

    Cycle Detection in Directed Graph init

---
+++ Graphs/Problems/DetectCycleD.cpp
@@ -0,0 +1,16 @@
+/* Cycle detection in Directed Graph */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool dfs_rec()
+
+    bool detect_cycle(vector<int> adj[], int v)
+{
+}
+
+int main()
+{
+
+    return 0;
+}
